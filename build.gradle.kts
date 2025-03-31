ae0bf7d9dd074afac0308d5dcbe3fef30eb6ad00
MicroEJ GitHub Delivery <delivery@microej.com>
2025-03-31 10:27:27 +0200

    Version 3.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,5 +7,5 @@
 
 allprojects {
     group = "com.microej.veeport.imx93"
-    version = "3.0.1"
+    version = "3.0.2"
 }
--- vee-port/build.gradle.kts
+++ vee-port/build.gradle.kts
@@ -5,14 +5,23 @@
  * Use of this source code is governed by a BSD-style license that can be found with this software.
  */
 
-import java.util.*
+import java.io.FileOutputStream
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
 
 plugins {
     id("com.microej.gradle.veeport") version libs.versions.microej.sdk
 }
 
+configurations {
+    create("compileClasspath")
+}
+
 microej {
     skippedCheckers = "changelog,readme,license"
+    withIvyDescriptor()
 }
 
 dependencies {
@@ -20,7 +29,7 @@ dependencies {
 
     microejPack(libs.pack.ui.architecture)
 
-//    Packs are fetched from Linux-AbstractionLayer/gradle/libs.versions.toml
+    // Packs are fetched from Linux-AbstractionLayer/gradle/libs.versions.toml
     microejPack(libsOverride.pack.device)
     microejPack(libsOverride.pack.ecom.network)
     microejPack(libsOverride.pack.fs)
@@ -28,10 +37,7 @@ dependencies {
 
     microejFrontPanel(project(":vee-port:front-panel"))
 
-//    microejMock(project(":vee-port:mock"))
     microejMock("com.is2t.microej.deploy:jenkins:1.0.2")
-
-//    microejTool(project(":vee-port:image-generator"))
 }
 
 tasks.register<Copy>("copyBspScripts") {
@@ -41,3 +47,50 @@ tasks.register<Copy>("copyBspScripts") {
 
 tasks.getByName("buildVeePort").dependsOn("copyBspScripts")
 tasks.getByName("buildVeePortConfiguration").dependsOn("copyBspScripts")
+
+tasks.register("updateIvyDescriptor") {
+    doLast {
+        val ivyFile = layout.buildDirectory.file("ivy.xml").get().asFile.absolutePath
+
+        val factory = DocumentBuilderFactory.newInstance()
+        val builder = factory.newDocumentBuilder()
+        val document = builder.parse(ivyFile)
+        val publicationsElements = document.getElementsByTagName("publications")
+        if (publicationsElements.length == 1) {
+            val publications = publicationsElements.item(0)
+
+            while (publications.hasChildNodes()) {
+                publications.removeChild(publications.firstChild)
+            }
+
+            var newPublication = document.createElement("artifact")
+            newPublication.setAttribute("name", "vee-port")
+            newPublication.setAttribute("type", "zip")
+            newPublication.setAttribute("ext", "zip")
+            newPublication.setAttribute("conf", "default")
+            publications.appendChild(newPublication)
+
+            newPublication = document.createElement("artifact")
+            newPublication.setAttribute("name", "vee-port")
+            newPublication.setAttribute("type", "pom")
+            newPublication.setAttribute("ext", "pom")
+            newPublication.setAttribute("conf", "dist")
+            publications.appendChild(newPublication)
+
+            newPublication = document.createElement("artifact")
+            newPublication.setAttribute("name", "vee-port")
+            newPublication.setAttribute("type", "module")
+            newPublication.setAttribute("ext", "module")
+            newPublication.setAttribute("conf", "dist")
+            publications.appendChild(newPublication)
+
+            val transformerFactory = TransformerFactory.newInstance()
+            val transformer = transformerFactory.newTransformer()
+            val source = DOMSource(document)
+            val result = StreamResult(FileOutputStream(ivyFile))
+            transformer.transform(source, result)
+        }
+    }
+}
+
+tasks.getByName("generateIvyDescriptor").finalizedBy("updateIvyDescriptor")
